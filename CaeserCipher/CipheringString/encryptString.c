3aa52367eeeeeab16fb402b65706c5872481d424
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-08 18:22:25 +0530

    ciphers a given text input or string by the user

--- CaeserCipher/CipheringString/encryptString.c
+++ CaeserCipher/CipheringString/encryptString.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*This program is used to encrypt a given string and return the cipher text to the user */
 void main()
 {
-    char str[50],ciph[50];
-    int ch;
+    char str[50],ciph[50]; //str for input and ciph for ciphered text
+    int ch; //to store each character
 
-    printf("Enter the string:");
-    fgets(str,50,stdin);
+    printf("Enter the string:"); //taking input
+    fgets(str,50,stdin); //fgets to take in the whole string 
     
-    int len = strlen(str);
-    if (str[len-1] == '\n'){
+    int len = strlen(str); //taking length of the string to use it below
+    
+    if (str[len-1] == '\n') //this removes the \n stored at the end when fgets is used
+    {
         str[len-1] = '\0';
     }
 
-    for(int i=0;i<strlen(str);i++)
+    for(int i=0;i<strlen(str);i++) //loop to take each character and cipher it
     {
         ch = str[i];
         ch = ch+3;
         ciph[i]=ch;
 
     }
-    puts(ciph);
+    puts(ciph); //printing the ciphered text
 
 }
