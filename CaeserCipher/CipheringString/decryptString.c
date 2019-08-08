5fa016c56a2a26981aa680bf92f220a7d74d4ea5
Jesswin W Varghese <jesswinw@gmail.com>
2019-08-08 18:20:43 +0530

    decrypts the cipher text made by encryptString.txt

--- CaeserCipher/CipheringString/decryptString.c
+++ CaeserCipher/CipheringString/decryptString.c
@@ -2,26 +2,29 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*This program is used to decrypt the string encrypted by the the encryptSting.c */
 void main()
 {
-    char str[50],deciph[50];
-    int ch;
+    char str[50],deciph[50]; //one for storing the ciphered string and one for the deciphered string
+    int ch; //used for storing each character in the loop
 
     printf("Enter the encrypted string:");
-    fgets(str,50,stdin);
+    fgets(str,50,stdin); //for taking the entire string
     
-    int len = strlen(str);
-    if (str[len-1] == '\n'){
+    int len = strlen(str); //taking length of the string to use it below
+
+    if (str[len-1] == '\n')//this removes the \n stored at the end when fgets is used
+    {
         str[len-1] = '\0';
     }
 
-    for(int i=0;i<strlen(str);i++)
+    for(int i=0;i<strlen(str);i++) // loop for taking each ciphered character and deciphering it 
     {
         ch = str[i];
         ch = ch-3;
         deciph[i]=ch;
 
     }
-    puts(deciph);
+    puts(deciph); //printing the deciphered text
 
 }
